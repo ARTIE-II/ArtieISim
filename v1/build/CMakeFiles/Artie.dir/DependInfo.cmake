2f41cf5f9ed6a3c4531a92785b5161062a17a4f2
Nicholas Carrara <ncarrara.physics@gmail.com>
2022-12-10 07:33:57 -0800

    added older sim version

---
+++ v1/build/CMakeFiles/Artie.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/Artie.cc" "CMakeFiles/Artie.dir/Artie.cc.o" "gcc" "CMakeFiles/Artie.dir/Artie.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/ActionInitialization.cc" "CMakeFiles/Artie.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/Artie.dir/src/ActionInitialization.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/DetectorConstruction.cc" "CMakeFiles/Artie.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/Artie.dir/src/DetectorConstruction.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/DetectorMessenger.cc" "CMakeFiles/Artie.dir/src/DetectorMessenger.cc.o" "gcc" "CMakeFiles/Artie.dir/src/DetectorMessenger.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/EventAction.cc" "CMakeFiles/Artie.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/Artie.dir/src/EventAction.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/HistoManager.cc" "CMakeFiles/Artie.dir/src/HistoManager.cc.o" "gcc" "CMakeFiles/Artie.dir/src/HistoManager.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/NeutronHPMessenger.cc" "CMakeFiles/Artie.dir/src/NeutronHPMessenger.cc.o" "gcc" "CMakeFiles/Artie.dir/src/NeutronHPMessenger.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/NeutronHPphysics.cc" "CMakeFiles/Artie.dir/src/NeutronHPphysics.cc.o" "gcc" "CMakeFiles/Artie.dir/src/NeutronHPphysics.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/PhysicsList.cc" "CMakeFiles/Artie.dir/src/PhysicsList.cc.o" "gcc" "CMakeFiles/Artie.dir/src/PhysicsList.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/PrimaryGeneratorAction.cc" "CMakeFiles/Artie.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/Artie.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/Run.cc" "CMakeFiles/Artie.dir/src/Run.cc.o" "gcc" "CMakeFiles/Artie.dir/src/Run.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/RunAction.cc" "CMakeFiles/Artie.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/Artie.dir/src/RunAction.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/StackingAction.cc" "CMakeFiles/Artie.dir/src/StackingAction.cc.o" "gcc" "CMakeFiles/Artie.dir/src/StackingAction.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/SteppingAction.cc" "CMakeFiles/Artie.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/Artie.dir/src/SteppingAction.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/SteppingVerbose.cc" "CMakeFiles/Artie.dir/src/SteppingVerbose.cc.o" "gcc" "CMakeFiles/Artie.dir/src/SteppingVerbose.cc.o.d"
+  "/home/ncarrara/physics/ARTIE/ArtieISim/v1/src/TrackingAction.cc" "CMakeFiles/Artie.dir/src/TrackingAction.cc.o" "gcc" "CMakeFiles/Artie.dir/src/TrackingAction.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ v1/build/CMakeFiles/Artie.dir/cmake_clean.cmake
@@ -0,0 +1,41 @@
+file(REMOVE_RECURSE
+  "Artie"
+  "Artie.pdb"
+  "CMakeFiles/Artie.dir/Artie.cc.o"
+  "CMakeFiles/Artie.dir/Artie.cc.o.d"
+  "CMakeFiles/Artie.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/Artie.dir/src/ActionInitialization.cc.o.d"
+  "CMakeFiles/Artie.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/Artie.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/Artie.dir/src/DetectorMessenger.cc.o"
+  "CMakeFiles/Artie.dir/src/DetectorMessenger.cc.o.d"
+  "CMakeFiles/Artie.dir/src/EventAction.cc.o"
+  "CMakeFiles/Artie.dir/src/EventAction.cc.o.d"
+  "CMakeFiles/Artie.dir/src/HistoManager.cc.o"
+  "CMakeFiles/Artie.dir/src/HistoManager.cc.o.d"
+  "CMakeFiles/Artie.dir/src/NeutronHPMessenger.cc.o"
+  "CMakeFiles/Artie.dir/src/NeutronHPMessenger.cc.o.d"
+  "CMakeFiles/Artie.dir/src/NeutronHPphysics.cc.o"
+  "CMakeFiles/Artie.dir/src/NeutronHPphysics.cc.o.d"
+  "CMakeFiles/Artie.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/Artie.dir/src/PhysicsList.cc.o.d"
+  "CMakeFiles/Artie.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/Artie.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/Artie.dir/src/Run.cc.o"
+  "CMakeFiles/Artie.dir/src/Run.cc.o.d"
+  "CMakeFiles/Artie.dir/src/RunAction.cc.o"
+  "CMakeFiles/Artie.dir/src/RunAction.cc.o.d"
+  "CMakeFiles/Artie.dir/src/StackingAction.cc.o"
+  "CMakeFiles/Artie.dir/src/StackingAction.cc.o.d"
+  "CMakeFiles/Artie.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/Artie.dir/src/SteppingAction.cc.o.d"
+  "CMakeFiles/Artie.dir/src/SteppingVerbose.cc.o"
+  "CMakeFiles/Artie.dir/src/SteppingVerbose.cc.o.d"
+  "CMakeFiles/Artie.dir/src/TrackingAction.cc.o"
+  "CMakeFiles/Artie.dir/src/TrackingAction.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Artie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
